7ab2592a850dd7c4435521e6c793ca58c58634a4
keren <kerenbog@gmail.com>
2020-09-25 15:42:06 +0300

    constructor created

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,22 +2,22 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        hello(" Keren");
+     hello(" Keren");
 
-     double len = 5;
-        System.out.println("square area with side"+ len+ "=" + area(len));
-     double a=6;
-     double b=7;
-        System.out.println("rectangle area with side "+a+" and "+"side "+b+ "=" + area(a,b));
+     Square s = new Square(5);
+     System.out.println("square area with side "+ s.l+ "=" + area(s));
 
+     Rectangle r = new Rectangle(6,7);
+     System.out.println("rectangle area with side "+ r.a+ " and "+"side " +r.b+ "="+ area(r));
     }
-    public static double area(double l) {
-         return  l*l;
+    public static double area(Square s) {
+         return  s.l* s.l;
+    }
+    public static double area(Rectangle r) {
+        return  r.a* r.b;
     }
 
-    public static double area (double a,double b){
-        return a*b;
-    }
+
 
     public static void hello(String somebody) {
         System.out.println("hello," + somebody + "!");
