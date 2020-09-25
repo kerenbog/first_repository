86b173c8e8bdc1e9a34ee67d5aae4afd0af64431
keren <kerenbog@gmail.com>
2020-09-25 19:07:07 +0300

    Д/З 2 пункт 4 - создан метод в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBTD.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBTD.java
@@ -3,13 +3,9 @@ package ru.stqa.pft.sandbox;
 public class DistanceBTD {
     public static void main(String[] args) {
 
-    Point p= new Point(5,2,7,4);
-        System.out.println( "the distance between dot P1"+"("+p.a+","+p.b+")"+" and "+ "dot P2"+"("+p.c+","+p.d+")"+" is "+
-                Distance(p));
+        Point p = new Point(5, 2, 7, 4);
+        System.out.println("the distance between dot P1" + "(" + p.a + "," + p.b + ")" + " and " + "dot P2" + "(" + p.c + "," + p.d + ")" + " is " +
+                p.Distance());
 
     }
-    public static double Distance(Point p){
-        return Math.sqrt((p.b-p.a)*(p.b-p.a)+(p.d-p.c)*(p.d-p.c));
-    }
-
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -6,11 +6,16 @@ public class Point {
     public double c;
     public double d;
 
-    public Point( double a,double b, double c, double d) {
-        this.a=a;
-        this.b=b;
-        this.c=c;
-        this.d=d;
+    public Point(double a, double b, double c, double d) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+    }
+
+    public double Distance() {
+        return Math.sqrt((this.b - this.a) * (this.b - this.a) + (this.d - this.c) * (this.d - this.c));
+
     }
 }
 
