b7dd8f9a2d8e28ecdd314da2b0a9f116d9300c07
keren <kerenbog@gmail.com>
2020-09-26 19:25:38 +0300

    добавлены тесты для класса Point

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class PointTests {
+    @Test
+
+    public void PointTest (){
+        Point k= new Point(5,2,7,4);
+        Assert.assertEquals(k.Distance(), 4.0);
+    }
+}
