519e741649b5a598baddd79228e94ced6ccd5f62
keren <kerenbog@gmail.com>
2020-09-26 19:05:19 +0300

    лобавила функцию Assert в класс SquareTests

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SquareTests {
@@ -7,7 +8,7 @@ public class SquareTests {
     @Test
     public void testArea(){
         Square g= new Square(5);
-        assert g.area()==25;
+        Assert.assertEquals(g.area(),25.0);
     }
 
 }
