1bdc23bcf6b543d733c45e3af326f2889ea539f8
keren <kerenbog@gmail.com>
2020-10-04 20:28:37 +0300

    исправлено задание 3

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBTD.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceBTD.java
@@ -2,8 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class DistanceBTD {
     public static void main(String[] args) {
-        Point P1= new Point(2,6);
-        Point P2 = new Point(4,1);
+        Point P1= new Point(5,6);
+        Point P2 = new Point(5,1);
         System.out.println(distance(P1,P2));
         System.out.println(P1.distance2(P2));
     }
--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -1,12 +1,22 @@
 package ru.stqa.pft.sandbox;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
 public class PointTests {
     @Test
+public void testDistance() {
 
-    public void PointTest (){
+        Point P1 = new Point(2, 6);
+        Point P2 = new Point(5, 1);
+        Assert.assertEquals(P1.distance2(P2),5);
+    }
+    @Test
+public void testDistance1() {
 
+        Point P1 = new Point(5, 6);
+        Point P2 = new Point(5, 1);
+        Assert.assertEquals(P1.distance2(P2),5);
     }
 }
